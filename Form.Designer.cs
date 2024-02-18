49891e3cdaa53bafd8dcf61a196d779c5d22765e
Lior Banai <liorbanai@gmail.com>
2024-02-18 21:51:19 +0200

    wip

---
+++ Form.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace ProcessDependency
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // Form
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(742, 514);
+            Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            Name = "Form";
+            Text = "Nuget Dependencies";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+    }
+}
+
---
+++ Form.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Msagl.Drawing;
+using Microsoft.Msagl.Layout.MDS;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.ServiceProcess;
+using Newtonsoft.Json;
+
+namespace ProcessDependency
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+        public Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            Microsoft.Msagl.GraphViewerGdi.GViewer viewer = new Microsoft.Msagl.GraphViewerGdi.GViewer();
+            Graph graph = new Graph("graph");
+            var json = File.ReadAllText("c:\\0\\project.assets.json");
+            var dependencies = JsonConvert.DeserializeObject(json);
+
+            foreach (ServiceController serviceController in ServiceController.GetServices())
+            {
+                var service = $"{serviceController.DisplayName} ({serviceController.ServiceName})";
+                graph.AddNode(service);
+                foreach (var dependentService in serviceController.DependentServices)
+                {
+                    string dependent = $"{dependentService.DisplayName} ({dependentService.ServiceName})";
+                    graph.AddNode(dependent);
+                    graph.AddEdge(dependent, service);
+                }
+            }
+            //bind the graph to the viewer 
+            graph.Attr.LayerDirection = LayerDirection.None;
+            graph.Attr.OptimizeLabelPositions = true;
+            graph.CreateLayoutSettings();
+            graph.LayoutAlgorithmSettings = new MdsLayoutSettings();
+            viewer.Graph = graph;
+            SuspendLayout();
+            viewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            Controls.Add(viewer);
+            ResumeLayout();
+
+        }
+    }
+}
